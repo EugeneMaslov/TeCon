caac4ff149b44214490c7c99f4185b86b685a920
Евгений <@HOME-PC>
2021-09-28 10:32:32 +0300

    Добавьте файлы проекта.

---
+++ TeCon/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TeCon
+{
+    public partial class MainPage : ContentPage
+    {
+        public const string HEADER = "TeCon";
+        public MainPage()
+        { 
+            InitializeComponent();
+        }
+
+        public void button1_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
